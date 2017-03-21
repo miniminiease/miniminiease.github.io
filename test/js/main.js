c3101646d021678c192cbc2ea4e9c3238d0b9f15
miniminiease <eastoung@gmail.com>
2017-03-21 19:42:58 +0800

    fix menu content show-up rule

--- test/js/main.js
+++ test/js/main.js
@@ -1,9 +1,12 @@
 var MainCtrl = function($scope, $location, $compile, $filter) {
    $scope.setShow = function( _item ) {
-      for(var i=0; i<$scope.menu_items.length; ++i) {
-         $scope.menu_items[i].show = false;
+      if(_item.show) _item.show = false;
+      else {
+         for(var i=0; i<$scope.menu_items.length; ++i) {
+            $scope.menu_items[i].show = false;
+         }
+         _item.show = true;
       }
-      _item.show = true;
    };
 
    /* Private */
